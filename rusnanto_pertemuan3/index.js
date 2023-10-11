39514724e9a90dc7ba02a51689e2c9ee1e4a9bd0
Rusnanto <147584983+Rusnanto24@users.noreply.github.com>
2023-10-11 16:27:50 +0700

    Add files via upload

---
+++ rusnanto_pertemuan3/index.js
@@ -0,0 +1,9 @@
+let nama = prompt('Hallow Sob!! Silakan Masukkan Nama Kamu')
+
+let benar = prompt (" Masukkan 1 jika Benar, masukkan selain 2 jika Salah")
+
+let cek = confirm("Apakah Kamu Yakin?")
+
+let hasil = benar == "1" ? "Benar"  : "Salah"
+
+alert(hasil)
